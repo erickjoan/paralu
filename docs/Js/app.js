da09e4849f02635381afb5490cc683824197b859
erickjoan <75285454+erickjoan@users.noreply.github.com>
2023-09-24 17:48:58 -0500

    Add files via upload

---
+++ docs/Js/app.js
@@ -0,0 +1,15 @@
+
+
+let Boton1 = document.getElementById("B1");
+Boton1.addEventListener('click', function() {
+  location.href="flor.html";
+})
+
+let Boton2 = document.getElementById("regreso");
+Boton2.addEventListener('click', function() {
+  location.href="index.html";
+})
+
+
+
+
